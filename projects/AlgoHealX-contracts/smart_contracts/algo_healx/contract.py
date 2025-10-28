857c84a519c4d13bcb04a1a1a6f9a1b48957ae24
nikshiptha2505 <nikshipthapenthala@gmail.com>
2025-10-28 06:16:20 +0530

    feat: initialize AlgoHealx ARC4 smart contract structure

--- projects/AlgoHealX-contracts/smart_contracts/algo_healx/contract.py
+++ projects/AlgoHealX-contracts/smart_contracts/algo_healx/contract.py
@@ -1,8 +1,13 @@
-from algopy import ARC4Contract, String
+from algopy import ARC4Contract, Account, Txn, BoxMap, UInt64, String, subroutine
 from algopy.arc4 import abimethod
 
+MAX_BATCHES = 100
+MAX_MANUFACTURERS = 50
 
 class AlgoHealx(ARC4Contract):
-    @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    owner: Account
+    government_address: Account
+    batch_count: UInt64
+    manufacturer_count: UInt64
+    manufacturers: BoxMap[UInt64, Account]
+    batches: BoxMap[UInt64, String]
