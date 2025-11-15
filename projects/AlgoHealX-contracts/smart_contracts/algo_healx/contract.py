aaea6b0f9a7f9a39149decd12c4345409581a2cc
nikshiptha2505 <nikshipthapenthala@gmail.com>
2025-11-16 01:01:00 +0530

    Algohealx Update contract.py

--- projects/AlgoHealX-contracts/smart_contracts/algo_healx/contract.py
+++ projects/AlgoHealX-contracts/smart_contracts/algo_healx/contract.py
@@ -1,32 +1,28 @@
 from algopy import ARC4Contract, arc4, Global, UInt64, Bytes, String, Account, Txn
+
 class DrugBatchContract(ARC4Contract):
-    def __init__(self):
-        super().__init__()
-        self.drug_batches = Global.Dict("drug_batches")
-
-    @arc4.method
-    def register_drug_batch(self, batch_id: String, manufacturer: String, manufacture_date: String, expiry_date: String, quantity: UInt64):
-        """
-        Registers a new drug batch with the given details.
-        """
-        if self.drug_batches.contains(batch_id):
-            raise Exception("Batch ID already exists.")
-        
-        batch_info = {
-            "manufacturer": manufacturer,
-            "manufacture_date": manufacture_date,
-            "expiry_date": expiry_date,
-            "quantity": quantity
-        }
-        
-        self.drug_batches[batch_id] = batch_info
-
-    @arc4.method
-    def get_drug_batch(self, batch_id: String) -> Bytes:
-        """
-        Retrieves the details of a drug batch by its ID.
-        """
-        if not self.drug_batches.contains(batch_id):
-            raise Exception("Batch ID does not exist.")
-        
-        return self.drug_batches[batch_id]
+    def __init__(self) -> None:
+        self.admin = Global.creator_address
+        self.producer = Account()
+        self.regulator = Account()
+        self.batch_id = String()
+        self.drug_name = String()
+        self.manufacturer = String()
+        self.manufacture_date = String()
+        self.expiry_date = String()
+        self.quantity = UInt64()
+        self.status = String("unregistered")
+        self.timestamp = UInt64()
+        self.reg_status = String("pending")
+        self.rej_reason = String()
+        self.compliance_score = UInt64()
+        self.approval_ts = UInt64()
+        self.sender = Account()
+        self.receiver = Account()
+        self.current_location = String()
+        self.transfer_count = UInt64()
+        self.last_transfer_ts = UInt64()
+        self.verif_count = UInt64()
+        self.is_authentic = UInt64(1)
+        self.last_verif_ts = UInt64()
+        self.qr_hash = Bytes()
